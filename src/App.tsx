01e9b74a33581b16ae214523a4f3f728ce7c5417
Jonathan Correa Paiva <jona.correapc@gmail.com>
2023-07-22 18:39:07 -0300

    update: migrate from boostrap to chackra

--- src/App.tsx
+++ src/App.tsx
@@ -1,74 +1,116 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import { useState, useEffect } from 'react'
-import './App.css'
-import { WordResult } from './types'
-import { InputGroup, Form, Button, Container, Stack, Row, Col, ButtonGroup } from 'react-bootstrap'
-import * as React from 'react'
-import { search } from './services/search_word'
+import { useState, useEffect } from 'react';
+import {
+  ChakraProvider,
+  Stack,
+  InputGroup,
+  Input,
+  Button,
+  Grid,
+  GridItem,
+  Text,
+  Center,
+  ColorModeScript,
+  extendTheme,
+} from '@chakra-ui/react';
 import ReactAudioPlayer from 'react-audio-player';
-import Meaning from './components/meaning'
+import Meaning from './components/meaning';
+import { WordResult } from './types';
+import { search } from './services/search_word';
+
+
+const config = {
+  initialColorMode: 'dark', // Puedes cambiarlo a 'light' si deseas el modo claro por defecto
+  useSystemColorMode: true, // Habilitar para permitir que los usuarios usen la configuración del sistema
+};
+
+const theme = extendTheme({ config });
+
 
 function App() {
-  const [word, setWord] = useState('')
-  const [result, setResult] = useState<WordResult>([])
-  const [audio, setAudio] = useState<string>('')
-  
-  useEffect(()=>{
+  const [word, setWord] = useState('');
+  const [result, setResult] = useState<WordResult>([]);
+  const [audio, setAudio] = useState<string>('');
+
+  useEffect(() => {
     console.log(word)
-    if(word == null || word === '') return;
+    if (word == null || word === '') return;
     search(word).then(word => setResult(word));
-    setAudio(result[0]?.phonetics[0].audio)
+    setAudio(result[0]?.phonetics[0].audio || '')
   }, [word]);
-  
-  const handleSearch = (e:React.SyntheticEvent<HTMLFormElement>) => {
+
+  const handleSearch = (e: React.SyntheticEvent<HTMLFormElement>) => {
     e.preventDefault();
-    let { searchWord } = e.target as typeof e.target & { searchWord: {value: string}}
+    let { searchWord } = e.target as typeof e.target & { searchWord: { value: string } }
     setWord(searchWord.value)
-  }
+  };
 
   return (
-    <Container fluid>
-      <Stack gap={2}>
-        <Form onSubmit={handleSearch}>
-          <InputGroup className="mb-3">
-            <Form.Control
-              name='searchWord'
-              placeholder="Type any word..."
-              aria-label="Type any word..."
-              aria-describedby="basic-addon2"
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <Center h="100vh" w="100vw">
+
+        <Stack spacing={2}>
+          <form onSubmit={handleSearch}>
+            <InputGroup>
+              <Input
+                name='searchWord'
+                placeholder="Type any word..."
+                aria-label="Type any word..."
+                aria-describedby="basic-addon2"
               />
-            <Button variant="outline-secondary" id="search-word" type='submit'>
-              Search
-            </Button>
-          </InputGroup>
-        </Form>
-          <Row>
-          <Col>
-            <p>{result[0]?.word}</p>
-            <p>{result[0]?.phonetics[0].text}</p>
-          </Col>
-          <Col> 
-          {audio !== ''?
-            <ReactAudioPlayer
-              id='audio-player'
-              src={audio}
-              autoPlay={false}
-            />:''
-          }
-          <ButtonGroup aria-label="Audio selection">
-            {result[0]?.phonetics.map(phonetic => {
-              return(
-                <Button variant={phonetic.audio === audio? "primary" : "secondary"} key={phonetic.audio} onClick={()=>{setAudio(phonetic.audio)}}>{phonetic.text}</Button>
-              )
-            })}
-          </ButtonGroup>
-          <Button onClick={() => {let audio = document.getElementById('audio-player') as HTMLAudioElement; audio.play()}}></Button>
-          </Col>
-          <Meaning meaning={result[0]?result[0].meanings[0]:null}></Meaning>
-          </Row>
-      </Stack>
-    </Container>
-  )
+              <Button type='submit' variant="outline">
+                Search
+              </Button>
+            </InputGroup>
+          </form>
+
+          {result.length === 0 ? (
+            <p>Loading...</p>
+          ) : (
+            <Grid templateColumns="repeat(2, 1fr)" gap={4}>
+              <GridItem>
+                <Text>{result[0]?.word}</Text>
+                <Text>{result[0]?.phonetics[0]?.text}</Text>
+              </GridItem>
+
+              <GridItem>
+                {audio !== '' ? (
+                  <ReactAudioPlayer id='audio-player' src={audio} autoPlay={false} />
+                ) : null}
+
+                <Button aria-label="Audio selection">
+                  {result[0]?.phonetics.map((phonetic) => (
+                    <Button
+                      key={phonetic.audio}
+                      onClick={() => {
+                        setAudio(phonetic.audio);
+                      }}
+                      variant={phonetic.audio === audio ? 'solid' : 'outline'}
+                    >
+                      {phonetic.text}
+                    </Button>
+                  ))}
+                </Button>
+                <Button
+                  onClick={() => {
+                    const audioElement = document.getElementById('audio-player') as HTMLAudioElement;
+                    if (audioElement && audio) {
+                      audioElement.play();
+                    }
+                  }}
+                  disabled={!audio}
+                >
+                  Play
+                </Button>
+              </GridItem>
+            </Grid>
+          )}
+          <Meaning meaning={result[0]?.meanings[0] || null} />
+        </Stack>
+      </Center>
+    </ChakraProvider>
+  );
 }
 
-export default App
+export default App;
+export { theme }
--- src/components/meaning.tsx
+++ src/components/meaning.tsx
@@ -1,47 +1,63 @@
-import { Meaning } from "../types"
-import { ListGroup } from 'react-bootstrap'
+import { Meaning as MeaningType } from "../types";
+import { Text, ListItem, OrderedList, Heading, Flex, Spacer, Box } from '@chakra-ui/react';
 
 type Props = {
-    meaning: Meaning
-    key?: string
-}
+    meaning: MeaningType | undefined;
+    key?: string;
+};
 
 function Meaning(props: Props) {
     const meaning = props.meaning;
-    return meaning ? (
+    if (!meaning) return null;
+
+    return (
         <div>
-            <p>{meaning.partOfSpeech}</p>
-            <h2>Meaning</h2>
-            <ListGroup as="ol" numbered>
-                {meaning.definitions.map(definition => {
-                    return (<ListGroup.Item
-                        key={definition.definition.slice(5)}
-                        as="li"
-                        className="d-flex justify-content-between align-items-start"
-                    >
-                        <div className="ms-2 me-auto">
-                            {definition.definition}
-                        </div>
-                    </ListGroup.Item>)
-                })}
-            </ListGroup>
-            {
-            meaning.synonyms.length === 0 ? null :
+            <Flex>
+                <Heading as="h4" size={"md"}>{meaning.partOfSpeech}</Heading>
+                <Spacer />
+            </Flex>
+            <Heading as="h5" size={"xs"}>Meaning</Heading>
+
+            <OrderedList as="ol" styleType="decimal">
+                {meaning.definitions.map((definition) => (
+                    <ListItem key={definition.definition.slice(5)}>
+                        <div className="ms-2 me-auto">{definition.definition}</div>
+                    </ListItem>
+                ))}
+            </OrderedList>
+
+            {meaning.synonyms.length === 0 ? null : (
                 <>
-                    <h3>Synonyms:</h3> 
-                    {meaning.synonyms.map((synonym) => {
-                        <h5 key={synonym}>{synonym}</h5>
-                    })}
+                    <Flex direction={"row"} justify="space-evenly">
+                        <Box>
+                            <Flex direction={"column"} justifyContent={"space-between"}>
+
+                                <Text>Synonyms:</Text>
+                            </Flex>
+                        </Box>
+                        <Box>
+                            {meaning.synonyms.map((synonym) => (
+                                <Text key={synonym}>{synonym}</Text>
+                            ))}
+                        </Box>
+                    </Flex>
                 </>
-            }
-            {meaning.antonyms.length === 0 ? null :
+            )}
+
+            {meaning.antonyms.length === 0 ? null : (
                 <>
-                    <h3>Antonyms:</h3> 
-                    {meaning.antonyms.map((antonym) => { (<p>{antonym}</p>) })}
+                    <Flex direction={"row"} justify="space-evenly">
+                        <Text as="h3">Antonyms:</Text>
+                        <Box>
+                            {meaning.antonyms.map((antonym) => (
+                                <Text key={antonym}>{antonym}</Text>
+                            ))}
+                        </Box>
+                    </Flex>
                 </>
-            }
+            )}
         </div>
-    ) : null
+    );
 }
 
-export default Meaning
+export default Meaning;
